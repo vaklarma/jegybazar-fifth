a029561b14887064e94878a89aac864d276f869a
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-09-23 18:27:15 +0200

    + index.html darabolása komponensekre           + navbar           + jumbotron           + eventcard           + footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { EventcardComponent } from './eventcard/eventcard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    EventcardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
