1b84ec4ba9042ece91f79089acef96de2a6106dd
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-09-28 22:47:44 +0200

    + Minden bekötése  + Sitebuild átemelése  + Children routerlinkek

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,34 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
-import {ProfileComponent} from './profile/profile.component';
-import {ProfileEditComponent} from './profile-edit/profile-edit.component';
-import {EventDetailComponent} from './event-detail/event-detail.component';
 import {EventListComponent} from './event-list/event-list.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AboutComponent} from './about/about.component';
 import {TicketComponent} from './ticket/ticket.component';
 import {EventComponent} from './event/event.component';
+import {EventDetailComponent} from './event-detail/event-detail.component';
+import {ProfileComponent} from './profile/profile.component';
+import {ProfileEditComponent} from './profile-edit/profile-edit.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'event', component: EventComponent},
+  {
+    path: 'event',
+    component: EventComponent,
+    children: [
+      {path: 'list', component: EventListComponent},
+      {path: 'new', component: EventDetailComponent},
+      {path: ':id/edit', component: EventDetailComponent},
+    ]
+  },
   {path: 'ticket', component: TicketComponent},
+  {
+    path: 'user',
+    children: [
+      {path: '', component: ProfileComponent},
+      {path: 'edit', component: ProfileEditComponent},
+    ]
+  },
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent},
@@ -34,10 +49,14 @@ export class AppRoutingModule {
   static routableComponents = [
     HomeComponent,
     EventComponent,
+    EventListComponent,
+    EventDetailComponent,
     TicketComponent,
     AboutComponent,
     LoginComponent,
     RegistrationComponent,
+    ProfileComponent,
+    ProfileEditComponent,
     PageNotFoundComponent,
   ];
 }
