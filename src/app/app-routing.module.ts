6cb2270dbf1260b0c77524d0b441aa6cd47f36e3
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-09-30 23:59:55 +0200

    Refaktorálás

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {RegistrationComponent} from './registration/registration.component';
-import {EventListComponent} from './event-list/event-list.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {LoginComponent} from './user/login/login.component';
+import {RegistrationComponent} from './user/registration/registration.component';
+import {EventListComponent} from './event/event-list/event-list.component';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 import {AboutComponent} from './about/about.component';
 import {TicketComponent} from './ticket/ticket.component';
 import {EventComponent} from './event/event.component';
-import {EventDetailComponent} from './event-detail/event-detail.component';
-import {ProfileComponent} from './profile/profile.component';
-import {ProfileEditComponent} from './profile-edit/profile-edit.component';
+import {EventDetailComponent} from './event/event-detail/event-detail.component';
+import {ProfileComponent} from './user/profile/profile.component';
+import {ProfileEditComponent} from './user/profile-edit/profile-edit.component';
+import {TicketListComponent} from './ticket/ticket-list/ticket-list.component';
+import {TicketDetailComponent} from './ticket/ticket-detail/ticket-detail.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -23,17 +25,25 @@ const routes: Routes = [
       {path: ':id/edit', component: EventDetailComponent},
     ]
   },
-  {path: 'ticket', component: TicketComponent},
+  {
+    path: 'ticket',
+    component: TicketComponent,
+    children: [
+      {path: 'list', component: TicketListComponent},
+      {path: 'new', component: TicketDetailComponent},
+      {path: ':id', component: TicketDetailComponent},
+    ]
+  },
   {
     path: 'user',
     children: [
       {path: '', component: ProfileComponent},
       {path: 'edit', component: ProfileEditComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'registration', component: RegistrationComponent},
     ]
   },
   {path: 'about', component: AboutComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent},
 
@@ -52,6 +62,8 @@ export class AppRoutingModule {
     EventListComponent,
     EventDetailComponent,
     TicketComponent,
+    TicketDetailComponent,
+    TicketListComponent,
     AboutComponent,
     LoginComponent,
     RegistrationComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {JumbotronComponent} from './jumbotron/jumbotron.component';
-import {EventcardComponent} from './eventcard/eventcard.component';
-import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './core/navbar/navbar.component';
+import {JumbotronComponent} from './core/jumbotron/jumbotron.component';
+import {EventcardComponent} from './event/eventcard/eventcard.component';
+import {FooterComponent} from './core/footer/footer.component';
 import {CollapseModule} from 'ngx-bootstrap';
 import { EventComponent } from './event/event.component';
 import {AppRoutingModule} from './app-routing.module';
