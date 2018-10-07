5f7b6616e0094586d570a469645555339a235d82
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-10-07 21:39:56 +0200

    User Model és User Service létrehozása Érdekesség hogy van egy statikus getter

---
+++ src/app/shared/user-model.ts
@@ -0,0 +1,25 @@
+export class UserModel {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  dateOfBirth: string;
+  gender: string;
+
+  constructor(param?: UserModel) {
+    if (param) {
+      Object.assign(this, param);
+    }
+  }
+
+  static get exampleUser(): UserModel {
+    return {
+      id: 0,
+      name: 'Legyek Réka Matilda',
+      email: 'legyekrekamatilda@valami.hu',
+      address: 'Futrinka utca',
+      dateOfBirth: '2015.01.08',
+      gender: 'male',
+    };
+  }
+}
