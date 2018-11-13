bef50bad9d0829f92618f8f458bf0cc6d9d3482d
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-11-13 23:12:10 +0100

    új jegy felvitele. ellenőrizve a minutustól

--- src/app/ticket/ticket-detail/ticket-detail.component.ts
+++ src/app/ticket/ticket-detail/ticket-detail.component.ts
@@ -3,6 +3,8 @@ import {EventService} from '../../shared/event.service';
 import {EventModel} from '../../shared/event-model';
 import {TicketService} from '../../shared/ticket.service';
 import {TicketModel} from '../../shared/ticket-model';
+import {UserService} from '../../shared/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-ticket-detail',
@@ -14,17 +16,23 @@ export class TicketDetailComponent implements OnInit {
   public ticket: TicketModel;
 
   constructor(private _eventService: EventService,
-              private _ticketService: TicketService) {
+              private _userService: UserService,
+              private _ticketService: TicketService,
+              private _router: Router) {
   }
 
   ngOnInit() {
     this.ticket = new TicketModel(TicketModel.emptyTicket);
+    this.ticket.sellerUserId = this._userService.getCurrentUser().id;
     this.events = this._eventService.getAllEvents();
   }
 
   onSubmit() {
     console.log('submitted');
     console.log(this.ticket);
+    this._ticketService.create(this.ticket);
+    this._router.navigate(['ticket']);
+
   }
 
 }
