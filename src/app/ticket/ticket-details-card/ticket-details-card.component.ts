0839b439b175f753c053941ac8898767fc7199c1
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-12-16 22:01:51 +0100

    Jegy részletek komponens

---
+++ src/app/ticket/ticket-details-card/ticket-details-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-details-card',
+  templateUrl: './ticket-details-card.component.html',
+  styleUrls: ['./ticket-details-card.component.css']
+})
+export class TicketDetailsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
