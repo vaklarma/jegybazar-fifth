9f81cb5909c48d1a7e28b91ce00584126532482f
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-12-16 14:13:52 +0100

    Néhány kommentes fg törlés Subscription figyelés

--- src/app/event/event-detail/event-detail.component.ts
+++ src/app/event/event-detail/event-detail.component.ts
@@ -58,31 +58,6 @@ export class EventDetailComponent implements OnInit, OnDestroy {
     this._destroy$.complete();
   }
 
-  // onSubmit() {
-  //
-  //   if (this.event.id) {
-  //     this._eventService.updateEvent(this.event)
-  //       .subscribe(
-  //         () => this.navigateBack()
-  //       );
-  //   } else {
-  //     this._eventService.createEvent(this.event)
-  //       .subscribe(
-  //         () => this.navigateBack()
-  //       );
-  //   }
-  //
-  //   //  this._eventService.save(this.event);
-  //
-  //   // this._eventService.save(this.event);
-  //
-  //   // .subscribe(
-  //   //   () => this.navigateBack(),
-  //   //   (err) => {
-  //   //     console.warn(`Problémánk van a form mentésnél: ${err}`);
-  //   //   }
-  //   // );
-  // }
 
   onSubmit() {
     this._eventService.save(this.event)
