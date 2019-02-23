4a4e00b47b86d8b910845956152da02dfc3a5596
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2019-02-24 00:22:33 +0100

    Itt van megvalósítva valóban

--- src/app/event/event-list/event-list.component.ts
+++ src/app/event/event-list/event-list.component.ts
@@ -1,9 +1,11 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {EventModel} from '../../shared/event-model';
 import {EventService} from '../../shared/event.service';
 import {UserService} from '../../shared/user.service';
-import {map} from 'rxjs/operators';
+import {BehaviorSubject, fromEvent} from 'rxjs';
+import {delay, distinctUntilChanged, flatMap, map} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-event-list',
@@ -11,7 +13,11 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./event-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EventListComponent implements OnInit {
+export class EventListComponent implements OnInit, AfterViewInit {
+  @ViewChild('searchInput') searchInput: ElementRef;
+  filteredText$ = new BehaviorSubject<string>(null);
+
+
 // ez jo pelda lehet smart es dumb componentre
 
   public eventsGrouppedBy3$: Observable<EventModel[][]>;
@@ -23,6 +29,24 @@ export class EventListComponent implements OnInit {
   ngOnInit() {
 
     this.eventsGrouppedBy3$ = this._eventService.getAllEvents()
+      .pipe(flatMap(
+        events => {
+          return this.filteredText$
+            .pipe(map(
+            filterText => {
+              if (filterText === null) {
+                return events;
+              } else {
+                return events.filter(
+                  event => {
+                    return event.name.toLocaleLowerCase().indexOf(filterText.toLowerCase()) > -1;
+                  }
+                );
+              }
+            }
+          ));
+        }
+      ))
       .pipe(map(data => {
         return data.reduce((acc: Array<any>, curr: EventModel, ind: number) => {
           if (ind % 3 === 0) {
@@ -32,6 +56,29 @@ export class EventListComponent implements OnInit {
           return acc;
         }, []);
       }));
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.searchInput);
+    fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(delay(600))
+      .pipe(
+        map(
+          (event: Event) => {
+            return (event.srcElement as HTMLInputElement).value;
+          }
+        )
+      )
+      .pipe(distinctUntilChanged())
+      .subscribe(
+        text => {
+          console.log(text);
+          if (text.length === 0) {
+            text = null;
+          }
+          this.filteredText$.next(text);
+        }
+      );
   }
 
 }
