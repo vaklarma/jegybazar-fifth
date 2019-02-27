5d496960b3accaa23f74ba3ba741bf4cd0e4c30b
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2019-02-27 22:42:47 +0100

    (add) create chat module and chat service and use dependence injection       for mocked chat service

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,6 +1,17 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChatService} from './chat.service';
+import {MockedChatService} from './mocked-chat.service';
+import {UserService} from '../shared/user.service';
+import {environment} from '../../environments/environment';
+
+export const chatServiceProvideFactoryFn =
+  (userService: UserService) => {
+    return environment.production ?
+      new ChatService(userService) :
+      new MockedChatService(userService);
+  };
+
 
 @NgModule({
   imports: [
@@ -9,10 +20,17 @@ import {ChatService} from './chat.service';
   declarations: []
 })
 export class ChatModule {
-static forRoot(): ModuleWithProviders {
-  return {
-    ngModule: ChatModule,
-    providers: [ChatService]
-  };
-}
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: ChatModule,
+      providers: [
+        {
+          provide: ChatService,
+          useFactory: chatServiceProvideFactoryFn,
+          deps: [UserService]
+
+        },
+      ]
+    };
+  }
 }
