1a92795638fc824ce385610b8f1d39a0ebeb9e78
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2019-04-27 22:16:46 +0200

    (add) Chat friend row hostlisteners to events

--- src/app/chat/chat-friend-row/chat-friend-row.component.ts
+++ src/app/chat/chat-friend-row/chat-friend-row.component.ts
@@ -1,19 +1,58 @@
-import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 import {ChatFriendModel} from '../model/chat-friend.model';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-chat-friend-row',
   templateUrl: './chat-friend-row.component.html',
   styleUrls: ['./chat-friend-row.component.css']
 })
-export class ChatFriendRowComponent implements OnInit {
+export class ChatFriendRowComponent implements OnInit, AfterViewInit {
   @Input() friend: ChatFriendModel;
   @HostBinding('class.clearfix') classClearFix = true;
+  @HostBinding('class.text-muted') classTextMuted = true;
+  @HostBinding('class.focused') classFocused = false;
 
-  constructor() {
+  private focus$ = new Subject<boolean>();
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.focus$
+      .subscribe(
+        value => {
+          if (value !== this.classTextMuted) {
+            this.classTextMuted = value;
+            this.classFocused = !value;
+          }
+        }
+      );
+  }
+
+  @HostListener('mouseover', ['$event'])
+  onHostFocus($event) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.focus$.next(false);
+  }
+
+  @HostListener('mouseout', ['$event'])
+  onHostBlur($event) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.focus$.next(true);
+  }
+
+  @HostListener('click', ['$event'])
+  onHostClick($event) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    console.log($event);
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.cdr.detach();
   }
 
 }
